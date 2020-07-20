eb4fcca461d5791875326669de91547f20bba6b1
Vinicius Santos <c.viniciussantos@outlook.com>
2020-07-20 14:01:24 -0300

    JPA repository, dependency injection, database seeding, (feito no commit anterior)

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -20,9 +20,9 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(user1, user2));
 	}
 }
